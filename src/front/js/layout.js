06a0087efa8f150aff9559b19dc0afbeee5f5154
AmiraMandi <amiramandisbaa@gmail.com>
2023-03-24 20:32:29 +0000

    se usado el link de react-dom

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,7 +10,8 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
-import { Widget } from "./component/widget.jsx"
+import { Widget } from "./component/widget.jsx";
+import  Login  from "./component/login.jsx";
 
 //create your first component
 const Layout = () => {
@@ -27,7 +28,7 @@ const Layout = () => {
           <NewsCards />
           <Routes>
             <Route element={<Home />} path="/home" />
-            <Route element={<Demo />} path="/demo" />
+            <Route element={<Login />} path="/login" />
             <Route element={<Single />} path="/single/:theid" />
             <Route element={<h1>Not found!</h1>} />
           </Routes>

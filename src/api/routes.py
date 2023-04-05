ffacdc3963d9e3487043b6f7ab2531695371776a
Georgi254 <georgi.gyovchev254@gmail.com>
2023-04-05 19:03:49 +0000

    Función de imágenes de las cards hecha

--- src/api/routes.py
+++ src/api/routes.py
@@ -528,7 +528,7 @@ def recuperarPassword():
 # Mediastack GET
 @api.route('/newsmediastack', methods=['GET'])
 def get_newsmediastack():
-    url = 'http://api.mediastack.com/v1/news?access_key=d3f508cbdee0f9fbedf32c471549f52b'
+    url = 'http://api.mediastack.com/v1/news?access_key=4a32d807e8f096e6c57661d4576ea097'
     category = request.args.get('category', default='business')
     keywords = request.args.get('keywords', default='')
     print("las categorias", category)
